2bc5d17003b7c93d8e67be3c727ed220101a47d5
Nasra <nasratullah_sarabi@yahoo.com>
2022-07-16 19:18:06 -0700

    Branch with Two class

---
+++ src/test/java/com/jUnit/nasrat2/Class2Nasrat.java
@@ -0,0 +1,10 @@
+package com.jUnit.nasrat2;
+
+import org.junit.jupiter.api.Test;
+
+public class Class2Nasrat {
+    @Test
+    public void class2Nasrat(){
+        System.out.println("commit2");
+    }
+}
