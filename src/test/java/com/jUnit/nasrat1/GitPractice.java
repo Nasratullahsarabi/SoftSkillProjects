368ba0b6629731467bbc25e38e7d2cf60d5929f8
Nasra <nasratullah_sarabi@yahoo.com>
2022-07-16 17:59:27 -0700

    Added gitignore and a package

---
+++ src/test/java/com/jUnit/nasrat1/GitPractice.java
@@ -0,0 +1,12 @@
+package com.jUnit.nasrat1;
+
+import com.selenium.utility.TestBase;
+import org.junit.jupiter.api.Test;
+
+public class GitPractice extends TestBase {
+
+    @Test
+    public void test1(){
+        System.out.println("To See what is this");
+    }
+}
