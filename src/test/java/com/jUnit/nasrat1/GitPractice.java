b83ae9c7711c75bc23c5bde5c82fe2309e204f67
Nasra <nasratullah_sarabi@yahoo.com>
2022-08-08 15:02:01 -0700

    Branch Strategy

--- src/test/java/com/jUnit/nasrat1/GitPractice.java
+++ src/test/java/com/jUnit/nasrat1/GitPractice.java
@@ -8,7 +8,6 @@ public class GitPractice extends TestBase {
     @Test
     public void test1(){
         System.out.println("To See what is this");
-        //A in Master
-        //C in Master
+        // To see how branch works
     }
 }
