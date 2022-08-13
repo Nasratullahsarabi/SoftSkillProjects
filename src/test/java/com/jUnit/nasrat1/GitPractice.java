36d4074a7f799d503a48f34cd13f7edc4273a0dd
Ateequllah Saraabi <ateequllah.saraabi@gmail.com>
2022-08-13 13:53:40 -0700

    joint branch conflict test3

--- src/test/java/com/jUnit/nasrat1/GitPractice.java
+++ src/test/java/com/jUnit/nasrat1/GitPractice.java
@@ -10,8 +10,8 @@ public class GitPractice extends TestBase {
         System.out.println("To See what is this");
         // To see how branch works
 
-        System.out.println("added");
-        System.out.println("Second");
+        System.out.println("hjkj3");
+        System.out.println("jkjhk3");
         System.out.println("New Changes");
         System.out.println("Today New branch");
         System.out.println("August 13");
